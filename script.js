3326c0d381c55ea4c92acbbccf19f579c2341c7b
0Cifher9 <0cifher9@gmail.com>
2024-06-03 17:14:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function showSection(sectionId) {
+    // Ocultar todas las secciones
+    var sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        section.style.display = 'none';
+    });
+
+    // Mostrar la sección seleccionada
+    var selectedSection = document.getElementById(sectionId);
+    selectedSection.style.display = 'block';
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log("¡Hola, Mundo! Este es un mensaje desde la consola.");
+    // Mostrar la sección Home por defecto
+    showSection('home');
 });
+
